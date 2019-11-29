dea8d2ac3d86ce99a68c1ccf6e80c7d2feb00a38
loiclu71 <56922441+loiclu71@users.noreply.github.com>
2019-11-29 18:38:55 +0100

    Update stairs.py

--- stairs.py
+++ stairs.py
@@ -4,7 +4,7 @@ import pygame
 from npc import Npc
 from consts import CELL_SIZE
 from print import *
-
+#We create the sign of upstairs and downstairs if the hero encounter these signs, he can go upstairs and downstairs.
 class UpStairs(Npc):
     images=load_images(["/Users/loic/PycharmProjects/game_again/images/Upstairs.png"])
 
